52cbf50d5a5e5f3ee144b8c3a005011d27c45d59
TChen09l <anhltgch210372@fpt.edu.vn>
2023-04-20 09:15:39 +0700

    setup blade view file and route web

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('aboutus');
+});
+
+Route::get('/detail/{id}', function () {
+    return view('cardetail');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/carsAdmin', function () {
+    return view('carsAdmin');
+});
+
+Route::get('/carsAdmin/create', function () {
+    return view('carsAdminCreate');
+});
+
+Route::get('/carsAdmin/edit/{id}', function () {
+    return view('carsAdminDetail');
 });
