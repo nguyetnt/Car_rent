111a5ca232e33a3820a266af97a6594340c5ec0c
tuananh-1 <anhldtgdh210341@fpt.edu.vn>
2023-04-20 15:17:14 +0700

    Initialize the project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Car Rent</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}">
 </head>
 <body>
-    
+    <nav>
+        <img src="{{ asset('assets/imgs/logo.jpg') }}" class="logo-img"/>
+    </nav>
 </body>
 </html>
