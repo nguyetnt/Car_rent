57f7138d1909500081d16c5679d9f02df8841b29
TChen09l <anhltgch210372@fpt.edu.vn>
2023-04-23 22:44:49 +0700

    edit view carsAdmin add css

---
+++ resources/views/carsAdmin.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/carsadmin.css') }}">
+    <title>carsAdmin</title>
+</head>
+<body>
+    <!-- Navbar -->
+    <div class="nav">
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/">HOME</a>
+              </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/about">ABOUT</a>
+            </li>
+            <li class="nav-item"></li>
+        </ul>
+        <ul class="nav-list">
+            <li class="nav-item">Đăng ký</li>
+            <li class="nav-item">Đăng nhập</li>
+        </ul>
+    </div>
+
+    <div class="Tiledanhsach">Danh sach cac xe co the cho nguoi dung thue </div>
+    
+    <!-- Column layout -->
+    <div class="column-layout">
+        <div class="column-item sidebar-one">
+            <img src="{{ asset('assets/imgs/download.jpg') }}" class="img-detail">
+            <h4 class="carID">Toyota Rush chuẩn </h4>
+            <button class="add">Thay doi thong tin</button>
+            <button class="add">Them vao muc cho thue</button>
+        </div>
+        <div class="column-item sidebar-one">
+            <img src="{{ asset('assets/imgs/download.jpg') }}" class="img-detail">
+            <h4 class="carID">Toyota Cross 1 8V chuẩn  </h4>
+            <button class="add">Thay doi thong tin</button>
+            <button class="add">Them vao muc cho thue</button>
+        </div>
+    <div class="column-item sidebar-one">
+        <img src="{{ asset('assets/imgs/download.jpg') }}" class="img-detail">
+            <h4 class="carID">Toyota Innova 2 0E chuẩn </h4>
+            <button class="add">Thay doi thong tin</button>
+            <button class="add">Them vao muc cho thue</button>
+        </div>
+    </div>
+     <!-- Column layout -->
+     <div class="column-layout">
+        <div class="column-item sidebar-one">
+            <img src="{{ asset('assets/imgs/download.jpg') }}" class="img-detail">
+            <h4 class="carID">Toyota Fortuner 24MT chuẩn </h4>
+            <button class="add">Thay doi thong tin</button>
+            <button class="add">Them vao muc cho thue</button>
+        </div>
+        <div class="column-item sidebar-one">
+            <img src="{{ asset('assets/imgs/download.jpg') }}" class="img-detail">
+            <h4 class="carID">Toyota Fortuner 2 4MT </h4>
+            <button class="add">Thay doi thong tin</button>
+            <button class="add">Them vao muc cho thue</button>
+        </div>
+    <div class="column-item sidebar-one">
+        <img src="{{ asset('assets/imgs/download.jpg') }}" class="img-detail">
+            <h4 class="carID">Volkswagen Teramont </h4>
+            <button class="add">Thay doi thong tin</button>
+            <button class="add">Them vao muc cho thue</button>
+        </div>
+    </div>
+     <!-- Column layout -->
+     <div class="column-layout">
+        <div class="column-item sidebar-one">
+            <img src="{{ asset('assets/imgs/download.jpg') }}" class="img-detail">
+            <h4 class="carID">Toyota Raize turbo </h4>
+            <button class="add">Thay doi thong tin</button>
+            <button class="add">Them vao muc cho thue</button>
+        </div>
+        <div class="column-item sidebar-one">
+            <img src="{{ asset('assets/imgs/download.jpg') }}" class="img-detail">
+            <h4 class="carID">VIOS2023 </h4>
+            <button class="add">Thay doi thong tin</button>
+            <button class="add">Them vao muc cho thue</button>
+        </div>
+    <div class="column-item sidebar-one">
+        <img src="{{ asset('assets/imgs/download.jpg') }}" class="img-detail">
+            <h4 class="carID">Kia Sorento GATH 24AT 2017 </h4>
+            <button class="add">Thay doi thong tin</button>
+            <button class="add">Them vao muc cho thue</button>
+        </div>
+    </div>
+</body>
+</html>
