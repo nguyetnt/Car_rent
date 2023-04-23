551219e1b8f96c1c6755d5a61ff8589e2a83f914
Thanhtt <thanhttgbh210727@fpt.edu.vn>
2023-04-23 23:11:43 +0700

    Edit view index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,26 +26,28 @@
   </ul>
 </nav>
 <main>
-  <h1>Tìm kiếm thuê xe</h1>
-  <div class="search-form">
+  <div class="search-container">
+    <h1>Tìm kiếm thuê xe</h1>
     <form>
-      <label for="city-search">Tìm theo thành phố:</label>
-      <input type="text" id="city-search" name="city-search" placeholder="Nhập tên thành phố">
-
-      <label for="location-search">Tìm theo địa điểm:</label>
-      <input type="text" id="location-search" name="location-search" placeholder="Nhập địa điểm">
-
-      <label for="rental-date">Thời gian thuê:</label>
-      <input type="datetime-local" id="rental-date" name="rental-date">
-
-      <label for="return-date">Thời gian trả xe:</label>
-      <input type="datetime-local" id="return-date" name="return-date">
-
+      <label for="country">Tìm theo quốc gia:</label>
+      <input type="text" id="country" name="country">
+      <label for="city">Tìm theo thành phố:</label>
+      <input type="text" id="city" name="city">
+      <label for="start-date">Thời gian thuê:</label>
+      <input type="datetime-local" id="start-date" name="start-date">
+      <label for="end-date">Thời gian trả xe:</label>
+      <input type="datetime-local" id="end-date" name="end-date">
+      <div class="checkbox-container">
+        <input type="checkbox" id="agree" name="agree">
+        <label for="agree">Tôi đồng ý với <a href="#">điều khoản thuê xe</a></label>
+      </div>
       <button type="submit">Tìm kiếm theo tiêu chí thuê</button>
     </form>
   </div>
+  <div class="image-container">
+    <img src="background.jpg" alt="car rental image">
+  </div>
 </main>
-
 
 </body>
 </html>
