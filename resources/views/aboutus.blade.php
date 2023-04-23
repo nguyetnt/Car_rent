c26457fae324f451876cd598595bb60c2b9a3c4d
TChen09l <anhltgch210372@fpt.edu.vn>
2023-04-23 10:05:34 +0700

    edit view aboutus add css and img

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <title>F8 - Shop</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{asset('assets/css/aboutus.css')}}">
+    </head>
+
+    <body class="">
+        <!-- Navbar -->
+        <div class="nav">
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="./index.php">HOME</a>
+                  </li>
+                <li class="nav-item">Kết nối</li>
+                <li class="nav-item">Tải ứng dụng</li>
+            </ul>
+            <ul class="nav-list">
+                <li class="nav-item">Đăng ký</li>
+                <li class="nav-item">Đăng nhập</li>
+            </ul>
+        </div>
+        <!-- Column layout -->
+        <div class="column-layout">
+            <div class="column-item sidebar-one">
+                <h2>Tiết kiệm phí thuê</h2>
+                <img src="{{ asset('assets/imgs/Rentcart.jpg') }}" class="logo-img">
+                <p>Giá thuê xe luôn công khai, tiết kiệm hơn 30% so với việc thuê xe truyền thống. Ngoài ra, khách thuê xe còn nhận được rất nhiều chương trình khuyến mãi.</p>
+            </div>
+            <div class="column-item main-column">
+                <h2>Chủ động tìm kiếm</h2>
+                <p>Lựa chọn những dòng xe, mức giá, thời gian,... theo lịch trình của mình với thông tin cụ thể của chiếc xe như biển số, hình ảnh, cấu hình đúng 100%.</p>
+            </div>
+            <div class="column-item sidebar-two">
+                <h2>Thuận tiện sử dụng</h2>
+                <p>Với thiết kế thông minh, ứng dụng giúp người dùng thuê xe để di chuyển trở nên dễ dàng, nhanh chóng và hiệu quả tại hơn 22 tỉnh/thành phố trên cả nước.</p>
+            </div>
+        </div>
+ <!-- Column layout -->
+        <div class="column-layout">
+            <div class="column-item sidebar-one">
+                <h2>An toàn trải nghiệm</h2>
+                <p>Khách thuê xe tại XeGo sẽ được hỗ trợ các gói bảo hiểm đa dạng, cung cấp bởi các công ty bảo hiểm uy tín hàng đầu tại Việt Nam.</p>
+            </div>
+            <div class="column-item main-column">
+                <h2>Chất lượng hàng đầu</h2>
+                <p>Chất lượng xe luôn được đảm bảo đúng yêu cầu cùng với thái độ phục vụ chuyên nghiệp.</p>
+            </div>
+            <div class="column-item sidebar-two">
+                <h2>Nhanh chóng hỗ trợ</h2>
+                <p>Đội ngũ Web luôn sẵn sàng hỗ trợ quý khách hàng 24/7.</p>
+            </div>
+        </div>
