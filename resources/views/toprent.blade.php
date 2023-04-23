07e028caf8e221e39fd7d7dbdffd37c3d782e2ba
tuananh-1 <anhldtgdh210341@fpt.edu.vn>
2023-04-23 21:42:05 +0700

    create view top rent

---
+++ resources/views/toprent.blade.php
@@ -0,0 +1,70 @@
+<?php 
+    include "./component/header.php"
+?>
+<style>
+    .c-toprent {
+        width: 100%;
+
+    }
+    
+    .row-1 {
+        width: 100%;
+        background-color: #78a4fa;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .row-2 {
+        height: 150px;
+        background-color: #d9d9d9;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .row-3 {
+        height: 50px;
+        background-color: #d9d9d9;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .row-2-5 {
+        width: 100%;
+        background-color: #78a4fa;
+        display: flex;
+        align-items: center;
+        padding: 30px;
+    }
+</style>
+<div class="c-toprent">
+    <div class="row-1">
+        <img src="https://res.cloudinary.com/cockbook/image/upload/v1682245265/single/image_4_1_vv9gyj.png" alt="">
+    </div>
+    <div class="row-2">
+        <div style="font-size: 24px; font-weight: 600">Top car rental</div>
+    </div>
+    <div class="row-2-5">
+        <a href="/car.php">
+            <img src="https://res.cloudinary.com/cockbook/image/upload/v1682245577/single/image_1_1_tdxidy.png" alt="">
+        </a>
+        <span style="font-size: 24px; margin-left: 30px">CAMARA - 12$/H</span>
+    </div>
+    <div class="row-3">
+        
+    </div>
+    <div class="row-2-5">
+        <span style="font-size: 24px; margin-left: 30px">AUDI - 10$/H</span>
+        <a href="/car.php">
+            <img style="margin-left: 30px" src="https://res.cloudinary.com/cockbook/image/upload/v1682246617/single/image_2_3_peleqs.png" alt="">
+        </a>
+    </div>
+    <div class="row-3">
+        
+    </div>
+    <div class="row-2-5">
+        <a href="/car.php">
+            <img src="https://res.cloudinary.com/cockbook/image/upload/v1682246653/single/image_3_1_tvclgp.png" alt="">
+        </a>
+        <span style="font-size: 24px; margin-left: 30px">CAMARA - 12$/H</span>
+    </div>
+</div>  
