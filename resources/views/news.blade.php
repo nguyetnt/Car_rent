dca75a34183aba882702c6c981f79f7492763f91
tuananh-1 <anhldtgdh210341@fpt.edu.vn>
2023-04-25 01:27:05 +0700

    edit css news toprent

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,96 +1,109 @@
-<link rel="stylesheet" href="{{ asset('assets/css/navbar.css')}}">
-<nav>
-    <ul class="nav-ul">
-    <li class="nav-link"><a class="nav-a" href="/">Trang chủ</a></li>
-    <li class="nav-link"><a class="nav-a" href="/toprent">Top xe được thuê</a></li>
-    <li class="nav-link"><a class="nav-a" href="/category">Các loại xe</a></li>
-    <li class="nav-link"><a class="nav-a" href="/news">Tin tức</a></li>
-    <li class="nav-link"><a class="nav-a" href="/contactus">Liên hệ</a></li>
-    <li class="nav-link"><a class="nav-a" href="/about">Thông tin</a></li>
-    <li class="nav-link"><a class="nav-a" href="/search">Tìm kiếm</a></li>
-    <li class="right-align"><a class="admin-btn" href="/carsAdmin">Quản lý</a></li>
-    <li class="right-align"><a class="login-btn" href="/login">Đăng nhập</a></li>
-    <li class="right-align"><a class="register-btn" href="/signup">Đăng ký</a></li>
-    </ul>
-</nav>
-
-<div class="new-container">
-    <div class="news-title">
-        <img src="https://res.cloudinary.com/cockbook/image/upload/v1682244050/single/image_1_ygsfns.png" alt="">
-        <span>News</span>
-    </div>
-    <div class="main-new-container">
-        <div class="inner-main-new-container">
-            <div class="inner-main-new-c-left">
-                <div class="highlight-new">
-                        <div class="hightlight-new-1">
-                            <img src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>News</title>
+    <link rel="stylesheet" href="{{asset('assets/css/news.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/navbar.css')}}">
+</head>
+<body>
+    <nav>
+        <ul class="nav-ul">
+        <li class="nav-link"><a class="nav-a" href="/">Trang chủ</a></li>
+        <li class="nav-link"><a class="nav-a" href="/toprent">Top xe được thuê</a></li>
+        <li class="nav-link"><a class="nav-a" href="/category">Các loại xe</a></li>
+        <li class="nav-link"><a class="nav-a" href="/news">Tin tức</a></li>
+        <li class="nav-link"><a class="nav-a" href="/contactus">Liên hệ</a></li>
+        <li class="nav-link"><a class="nav-a" href="/about">Thông tin</a></li>
+        <li class="nav-link"><a class="nav-a" href="/search">Tìm kiếm</a></li>
+        <li class="right-align"><a class="admin-btn" href="/carsAdmin">Quản lý</a></li>
+        <li class="right-align"><a class="login-btn" href="/login">Đăng nhập</a></li>
+        <li class="right-align"><a class="register-btn" href="/signup">Đăng ký</a></li>
+        </ul>
+    </nav> 
+    <div class="new-container">
+        <div class="news-title">
+            <img src="https://res.cloudinary.com/cockbook/image/upload/v1682244050/single/image_1_ygsfns.png" alt="">
+            <span>News</span>
+        </div>
+        <div class="main-new-container">
+            <div class="inner-main-new-container">
+                <div class="inner-main-new-c-left">
+                    <div class="highlight-new">
+                            <div class="hightlight-new-1">
+                                <img src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
+                            </div>
+                        <div class="hightlight-new-2">
+                            <a href="/news.php">
+                                <div style="width: 100%; height: 50%">
+                                    <img src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
+                                </div>
+    
+                            </a>
+                            <a href="/news.php">
+                                <div style="width: 100%; height: 50%">
+                                    <img src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
+                                </div>
+    
+                            </a>
                         </div>
-                    <div class="hightlight-new-2">
-                        <a href="/news.php">
-                            <div style="width: 100%; height: 50%">
-                                <img src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
-                            </div>
-
-                        </a>
-                        <a href="/news.php">
-                            <div style="width: 100%; height: 50%">
-                                <img src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
-                            </div>
-
-                        </a>
                     </div>
-                </div>
-                <br />
-                <div class="c-new">
+                    <br />
+                    <div class="c-new">
+                        <a href="/news.php">
+                            <div class="c-new-image">
+                                <img style="width: 100%" src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
+                            </div>
+                        </a>
+                        <div>
+                            <div style="font-weight: 600; margin-bottom: 12px">Test 123</div>
+                            <div>News</div>
+                        </div>
+                    </div>
+                    <div class="c-new">
                     <a href="/news.php">
                         <div class="c-new-image">
                             <img style="width: 100%" src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
                         </div>
+    
                     </a>
-                    <div>
-                        <div style="font-weight: 600; margin-bottom: 12px">Test 123</div>
-                        <div>News</div>
+                        <div>
+                            <div style="font-weight: 600; margin-bottom: 12px">Test 123</div>
+                            <div>News</div>
+                        </div>
                     </div>
                 </div>
-                <div class="c-new">
-                <a href="/news.php">
-                    <div class="c-new-image">
-                        <img style="width: 100%" src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
+                <div class="inner-main-new-c-right">
+                    <div class="news-title">
+                        <img src="https://res.cloudinary.com/cockbook/image/upload/v1682245129/single/image_3_ayzc2m.png" alt="">
+                        <span style="text-align: center;">Feature news</span>
                     </div>
-
-                </a>
-                    <div>
-                        <div style="font-weight: 600; margin-bottom: 12px">Test 123</div>
-                        <div>News</div>
+                    <br />
+                    <div class="c-new">
+                        <div class="c-new-image">
+                            <img style="width: 100%" src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
+                        </div>
+                        <div>
+                            <div style="font-weight: 600; margin-bottom: 12px">Test 123</div>
+                            <div>News</div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="inner-main-new-c-right">
-                <div class="news-title">
-                    <img src="https://res.cloudinary.com/cockbook/image/upload/v1682245129/single/image_3_ayzc2m.png" alt="">
-                    <span style="text-align: center;">Feature news</span>
-                </div>
-                <br />
-                <div class="c-new">
-                    <div class="c-new-image">
-                        <img style="width: 100%" src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
-                    </div>
-                    <div>
-                        <div style="font-weight: 600; margin-bottom: 12px">Test 123</div>
-                        <div>News</div>
-                    </div>
-                </div>
-                <div class="c-new">
-                    <div class="c-new-image">
-                        <img style="width: 100%" src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
-                    </div>
-                    <div>
-                        <div style="font-weight: 600; margin-bottom: 12px">Test 123</div>
-                        <div>News</div>
+                    <div class="c-new">
+                        <div class="c-new-image">
+                            <img style="width: 100%" src="https://res.cloudinary.com/cockbook/image/upload/v1682244330/single/Rectangle_rywiys.png" alt="">
+                        </div>
+                        <div>
+                            <div style="font-weight: 600; margin-bottom: 12px">Test 123</div>
+                            <div>News</div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</body>
+</html>
+ 
+
--- resources/views/toprent.blade.php
+++ resources/views/toprent.blade.php
@@ -1,81 +1,60 @@
-<nav>
-    <ul class="nav-ul">
-    <li class="nav-link"><a class="nav-a" href="/">Trang chủ</a></li>
-    <li class="nav-link"><a class="nav-a" href="/toprent">Top xe được thuê</a></li>
-    <li class="nav-link"><a class="nav-a" href="/category">Các loại xe</a></li>
-    <li class="nav-link"><a class="nav-a" href="/news">Tin tức</a></li>
-    <li class="nav-link"><a class="nav-a" href="/contactus">Liên hệ</a></li>
-    <li class="nav-link"><a class="nav-a" href="/about">Thông tin</a></li>
-    <li class="nav-link"><a class="nav-a" href="/search">Tìm kiếm</a></li>
-    <li class="right-align"><a class="admin-btn" href="/carsAdmin">Quản lý</a></li>
-    <li class="right-align"><a class="login-btn" href="/login">Đăng nhập</a></li>
-    <li class="right-align"><a class="register-btn" href="/signup">Đăng ký</a></li>
-    </ul>
-</nav>
-<style>
-    .c-toprent {
-        width: 100%;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Top rent car</title>
+    <link rel="stylesheet" href="{{asset('assets/css/toprent.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/navbar.css')}}">
+</head>
+<body>
+    <nav>
+        <ul class="nav-ul">
+        <li class="nav-link"><a class="nav-a" href="/">Trang chủ</a></li>
+        <li class="nav-link"><a class="nav-a" href="/toprent">Top xe được thuê</a></li>
+        <li class="nav-link"><a class="nav-a" href="/category">Các loại xe</a></li>
+        <li class="nav-link"><a class="nav-a" href="/news">Tin tức</a></li>
+        <li class="nav-link"><a class="nav-a" href="/contactus">Liên hệ</a></li>
+        <li class="nav-link"><a class="nav-a" href="/about">Thông tin</a></li>
+        <li class="nav-link"><a class="nav-a" href="/search">Tìm kiếm</a></li>
+        <li class="right-align"><a class="admin-btn" href="/carsAdmin">Quản lý</a></li>
+        <li class="right-align"><a class="login-btn" href="/login">Đăng nhập</a></li>
+        <li class="right-align"><a class="register-btn" href="/signup">Đăng ký</a></li>
+        </ul>
+    </nav>
+    <div class="c-toprent">
+        <div class="row-1">
+            <img src="https://res.cloudinary.com/cockbook/image/upload/v1682245265/single/image_4_1_vv9gyj.png" alt="">
+        </div>
+        <div class="row-2">
+            <div style="font-size: 24px; font-weight: 600">Top car rental</div>
+        </div>
+        <div class="row-2-5">
+            <a href="/car.php">
+                <img src="https://res.cloudinary.com/cockbook/image/upload/v1682245577/single/image_1_1_tdxidy.png" alt="">
+            </a>
+            <span style="font-size: 24px; margin-left: 30px">NAME CAR_Price/H</span>
+        </div>
+        <div class="row-3">
+            
+        </div>
+        <div class="row-2-5">
+            <span style="font-size: 24px; margin-left: 30px">NAME CAR_Price/H</span>
+            <a href="/car.php">
+                <img style="margin-left: 30px" src="https://res.cloudinary.com/cockbook/image/upload/v1682246617/single/image_2_3_peleqs.png" alt="">
+            </a>
+        </div>
+        <div class="row-3">
+            
+        </div>
+        <div class="row-2-5">
+            <a href="/car.php">
+                <img src="https://res.cloudinary.com/cockbook/image/upload/v1682246653/single/image_3_1_tvclgp.png" alt="">
+            </a>
+            <span style="font-size: 24px; margin-left: 30px">NAME CAR_Price/H</span>
+        </div>
+    </div>  
+</body>
+</html>
 
-    }
-    
-    .row-1 {
-        width: 100%;
-        background-color: #78a4fa;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .row-2 {
-        height: 150px;
-        background-color: #d9d9d9;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .row-3 {
-        height: 50px;
-        background-color: #d9d9d9;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .row-2-5 {
-        width: 100%;
-        background-color: #78a4fa;
-        display: flex;
-        align-items: center;
-        padding: 30px;
-    }
-</style>
-<div class="c-toprent">
-    <div class="row-1">
-        <img src="https://res.cloudinary.com/cockbook/image/upload/v1682245265/single/image_4_1_vv9gyj.png" alt="">
-    </div>
-    <div class="row-2">
-        <div style="font-size: 24px; font-weight: 600">Top car rental</div>
-    </div>
-    <div class="row-2-5">
-        <a href="/car.php">
-            <img src="https://res.cloudinary.com/cockbook/image/upload/v1682245577/single/image_1_1_tdxidy.png" alt="">
-        </a>
-        <span style="font-size: 24px; margin-left: 30px">NAME CAR_Price/H</span>
-    </div>
-    <div class="row-3">
-        
-    </div>
-    <div class="row-2-5">
-        <span style="font-size: 24px; margin-left: 30px">NAME CAR_Price/H</span>
-        <a href="/car.php">
-            <img style="margin-left: 30px" src="https://res.cloudinary.com/cockbook/image/upload/v1682246617/single/image_2_3_peleqs.png" alt="">
-        </a>
-    </div>
-    <div class="row-3">
-        
-    </div>
-    <div class="row-2-5">
-        <a href="/car.php">
-            <img src="https://res.cloudinary.com/cockbook/image/upload/v1682246653/single/image_3_1_tvclgp.png" alt="">
-        </a>
-        <span style="font-size: 24px; margin-left: 30px">NAME CAR_Price/H</span>
-    </div>
-</div>  
