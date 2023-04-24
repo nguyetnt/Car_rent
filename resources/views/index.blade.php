cebe73489b5a39ae13c4676b0d60240df3be6aa6
Thanhtt <thanhttgbh210727@fpt.edu.vn>
2023-04-24 18:21:39 +0700

    update navbar css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,12 +11,12 @@
 <body>
 
 <nav>
-  <ul>
-    <li><a href="#">Trang chủ</a></li>
-    <li><a href="#">Top xe được thuê</a></li>
-    <li><a href="#">Các loại xe</a></li>
-    <li><a href="#">Tin tức</a></li>
-    <li><a href="#">Liên hệ</a></li>
+  <ul class="nav-ul">
+    <li class="nav-link"><a class="nav-a" href="#">Trang chủ</a></li>
+    <li class="nav-link"><a class="nav-a" href="#">Top xe được thuê</a></li>
+    <li class="nav-link"><a class="nav-a" href="#">Các loại xe</a></li>
+    <li class="nav-link"><a class="nav-a" href="#">Tin tức</a></li>
+    <li class="nav-link"><a class="nav-a" href="#">Liên hệ</a></li>
     <li class="right-align"><a href="#" class="admin-btn">Quản lý</a></li>
     <li class="right-align"><a href="#" class="login-btn">Đăng nhập</a></li>
     <li class="right-align"><a href="#" class="register-btn">Đăng ký</a></li>
