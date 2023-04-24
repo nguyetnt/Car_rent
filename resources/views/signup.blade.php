d7969b811c6e6a9e96ca505926a6af1b1b5f8751
tuananh-1 <anhldtgdh210341@fpt.edu.vn>
2023-04-25 01:36:04 +0700

    edit css login signup

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,75 +1,64 @@
-<style>
-    * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-
-    .c-wrap-signup {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .w-signup {
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .inner-w-signup {
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        gap: 16px;
-        max-width: 500px;
-    }
-
-    .c-flex-center {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: #fff;
-    }
-</style>
-
-<div class="c-signup">
-    <div class="c-wrap-signup">
-        <img src="https://res.cloudinary.com/cockbook/image/upload/v1682247687/single/image_2_5_t9axtg.png" alt="" />
-    </div>
-    <br />
-    <div class="w-signup">
-        <div class="inner-w-signup">
-            <div style="font-size: 24px; font-weight: 600">Create your account</div>
-            <div style="width: 100%;">
-                <div style="margin-bottom: 8px;">Username</div>
-                <input type="text" style="width: 100%; height: 50px; background-color: #d9d9d9">
-            </div>
-            <div style="width: 100%;">
-                <div style="margin-bottom: 8px;">Password</div>
-                <input type="password" style="width: 100%; height: 50px; background-color: #d9d9d9">
-            </div>
-            <div style="width: 100%;">
-                <div style="margin-bottom: 8px;">Retype password</div>
-                <input type="password" style="width: 100%; height: 50px; background-color: #d9d9d9">
-            </div>
-            <div style="width: 100%;">
-                <div style="margin-bottom: 8px;">Email</div>
-                <input type="text" style="width: 100%; height: 50px; background-color: #d9d9d9">
-            </div>
-            <div style="width: 100%;">
-                <div style="margin-bottom: 8px;">Phone number</div>
-                <input type="text" style="width: 100%; height: 50px; background-color: #d9d9d9">
-            </div>
-            <div style="width: 100%; height: 50px; background-color: #377BB9C9;" class="c-flex-center">
-                Sign up</div>
-            <div>Do you have already have an account? Log in</div>
-            <br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign up</title>
+    <link rel="stylesheet" href="{{asset('assets/css/signup.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/navbar.css')}}">
+</head>
+<body>
+    <nav>
+        <ul class="nav-ul">
+        <li class="nav-link"><a class="nav-a" href="/">Trang chủ</a></li>
+        <li class="nav-link"><a class="nav-a" href="/toprent">Top xe được thuê</a></li>
+        <li class="nav-link"><a class="nav-a" href="/category">Các loại xe</a></li>
+        <li class="nav-link"><a class="nav-a" href="/news">Tin tức</a></li>
+        <li class="nav-link"><a class="nav-a" href="/contactus">Liên hệ</a></li>
+        <li class="nav-link"><a class="nav-a" href="/about">Thông tin</a></li>
+        <li class="nav-link"><a class="nav-a" href="/search">Tìm kiếm</a></li>
+        <li class="right-align"><a class="admin-btn" href="/carsAdmin">Quản lý</a></li>
+        <li class="right-align"><a class="login-btn" href="/login">Đăng nhập</a></li>
+        <li class="right-align"><a class="register-btn" href="/signup">Đăng ký</a></li>
+        </ul>
+    </nav>
+    <div class="c-signup">
+        <div class="c-wrap-signup">
+            <img src="https://res.cloudinary.com/cockbook/image/upload/v1682247687/single/image_2_5_t9axtg.png" alt="" />
         </div>
+        <br />
+        <div class="w-signup">
+            <div class="inner-w-signup">
+                <div style="font-size: 24px; font-weight: 600">Create your account</div>
+                <div style="width: 100%;">
+                    <div style="margin-bottom: 8px;">Username</div>
+                    <input type="text" style="width: 100%; height: 50px; background-color: #d9d9d9">
+                </div>
+                <div style="width: 100%;">
+                    <div style="margin-bottom: 8px;">Password</div>
+                    <input type="password" style="width: 100%; height: 50px; background-color: #d9d9d9">
+                </div>
+                <div style="width: 100%;">
+                    <div style="margin-bottom: 8px;">Retype password</div>
+                    <input type="password" style="width: 100%; height: 50px; background-color: #d9d9d9">
+                </div>
+                <div style="width: 100%;">
+                    <div style="margin-bottom: 8px;">Email</div>
+                    <input type="text" style="width: 100%; height: 50px; background-color: #d9d9d9">
+                </div>
+                <div style="width: 100%;">
+                    <div style="margin-bottom: 8px;">Phone number</div>
+                    <input type="text" style="width: 100%; height: 50px; background-color: #d9d9d9">
+                </div>
+                <div style="width: 100%; height: 50px; background-color: #377BB9C9;" class="c-flex-center">
+                    Sign up</div>
+                <div>Do you have already have an account? Log in</div>
+                <br>
+            </div>
+        </div>
+    
     </div>
+</body>
+</html>
 
-</div>
