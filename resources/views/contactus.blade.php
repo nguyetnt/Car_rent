88bfd4f3b7db44bbe296c9d2d4df7792a4db78b1
Thanhtt <thanhttgbh210727@fpt.edu.vn>
2023-04-24 19:01:01 +0700

    edit view category and css

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -5,24 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('assets/css/contactus.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/navbar.css')}}">
     <title>Contact us</title>
 </head>
+
 <body>
-    
-    <div class="container">
-        <section class="contact">
-          <h2>Liên hệ với chúng tôi</h2>
-          <form action="submit.php" method="post">
-            <label for="name">Họ và tên:</label>
-            <input type="text" id="name" name="name" required>
-            <label for="email">Địa chỉ email:</label>
-            <input type="email" id="email" name="email" required>
-            <label for="message">Tin nhắn:</label>
-            <textarea id="message" name="message" required></textarea>
-            <button type="submit">Gửi</button>
-          </form>
-        </section>
-      </div>
-      
+  
+  <nav>
+    <ul class="nav-ul">
+      <li class="nav-link"><a class="nav-a" href="#">Trang chủ</a></li>
+      <li class="nav-link"><a class="nav-a" href="#">Top xe được thuê</a></li>
+      <li class="nav-link"><a class="nav-a" href="#">Các loại xe</a></li>
+      <li class="nav-link"><a class="nav-a" href="#">Tin tức</a></li>
+      <li class="nav-link"><a class="nav-a" href="#">Liên hệ</a></li>
+      <li class="right-align"><a href="#" class="admin-btn">Quản lý</a></li>
+      <li class="right-align"><a href="#" class="login-btn">Đăng nhập</a></li>
+      <li class="right-align"><a href="#" class="register-btn">Đăng ký</a></li>
+    </ul>
+  </nav>
+
+  <div class="container">
+		<h2>Liên hệ với chúng tôi</h2>
+		<form>
+			<label for="name">Tên</label>
+			<input type="text" id="name" name="name" placeholder="Nhập tên của bạn">
+
+			<label for="phone">Số điện thoại</label>
+			<input type="text" id="phone" name="phone" placeholder="Nhập số điện thoại của bạn">
+
+			<label for="email">Email</label>
+			<input type="text" id="email" name="email" placeholder="Nhập email của bạn">
+
+			<label for="note">Ghi chú</label>
+			<textarea id="note" name="note" placeholder="Nhập ghi chú của bạn"></textarea>
+
+			<button type="submit">Gửi</button>
+		</form>
+
+		<div class="social-icons">
+			<a href="#"><i class="fab fa-facebook-f"></i></a>
+			<a href="#"><i class="fab fa-instagram"></i></a>
+			<a href="#"><i class="fab fa-tiktok"></i></a>
+		</div>
+	</div>
+
 </body>
 </html>
