da8c16b620390bba2c26d5e28a2ad5a0133f249d
thanhttgbh210727 <thanhttgbh210727@fpt.edu.vn>
2023-04-25 15:02:32 +0700

    edit html

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -31,7 +31,7 @@
 		<h3>Xe 4 chỗ</h3>
 		<div class="car-list">
 			<div class="car-card">
-				<img src="https://via.placeholder.com/300x200.png?text=Xe+1" alt="Xe 1">
+				<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+1" alt="Xe 1"></a>
 				<div class="car-info">
 					<h4>Xe 1</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -39,7 +39,7 @@
 				</div>
 			</div>
 			<div class="car-card">
-				<img src="https://via.placeholder.com/300x200.png?text=Xe+2" alt="Xe 2">
+				<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+2" alt="Xe 2"></a>
 				<div class="car-info">
 					<h4>Xe 2</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -47,7 +47,7 @@
 				</div>
 			</div>
 			<div class="car-card">
-				<img src="https://via.placeholder.com/300x200.png?text=Xe+3" alt="Xe 3">
+				<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+3" alt="Xe 3"></a>
 				<div class="car-info">
 					<h4>Xe 3</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -55,7 +55,7 @@
 				</div>
 			</div>
 			<div class="car-card">
-                <img src="https://via.placeholder.com/300x200.png?text=Xe+4" alt="Xe 4">
+                <a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+4" alt="Xe 4"></a>
                 <div class="car-info">
                     <h4>Xe 4</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -63,7 +63,7 @@
                 </div>
             </div>
             <div class="car-card">
-                <img src="https://via.placeholder.com/300x200.png?text=Xe+5" alt="Xe 5">
+                <a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+5" alt="Xe 5"></a>
                 <div class="car-info">
                     <h4>Xe 5</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -74,7 +74,7 @@
         <h3>Xe 5 chỗ</h3>
 	<div class="car-list">
 		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+6" alt="Xe 6">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+6" alt="Xe 6"></a>
 			<div class="car-info">
 				<h4>Xe 6</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -82,7 +82,7 @@
 			</div>
 		</div>
 		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+7" alt="Xe 7">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+7" alt="Xe 7"></a>
 			<div class="car-info">
 				<h4>Xe 7</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -90,7 +90,7 @@
 			</div>
 		</div>
 		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+8" alt="Xe 8">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+8" alt="Xe 8"></a>
 			<div class="car-info">
 				<h4>Xe 8</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -98,7 +98,7 @@
 			</div>
 		</div>
 		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+9" alt="Xe 9">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+9" alt="Xe 9"></a>
 			<div class="car-info">
 				<h4>Xe 9</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -106,7 +106,7 @@
 			</div>
 		</div>
 		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+10" alt="Xe 10">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+10" alt="Xe 10"></a>
 			<div class="car-info">
 				<h4>Xe 10</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -118,52 +118,45 @@
 	<h3>Xe 7 chỗ</h3>
 	<div class="car-list">
 		<div class="car-card">
-        <img src="https://via.placeholder.com/300x200.png?text=Xe+10" alt="Xe 10">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+10" alt="Xe 11"></a>
 			<div class="car-info">
-				<h4>Xe 10</h4>
+				<h4>Xe 11</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				<p class="price">Giá thuê: $200/ngày</p>
 			</div>
 		</div>
 		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+11" alt="Xe 11">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+11" alt="Xe 12"></a>
 			<div class="car-info">
-				<h4>Xe 11</h4>
+				<h4>Xe 12</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				<p class="price">Giá thuê: $220/ngày</p>
 			</div>
 		</div>
 		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+12" alt="Xe 12">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+13" alt="Xe 13"></a>
 			<div class="car-info">
-				<h4>Xe 12</h4>
+				<h4>Xe 13</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				<p class="price">Giá thuê: $250/ngày</p>
 			</div>
 		</div>
 		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+13" alt="Xe 13">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+14" alt="Xe 14"></a>
 			<div class="car-info">
-				<h4>Xe 13</h4>
+				<h4>Xe 14</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				<p class="price">Giá thuê: $280/ngày</p>
 			</div>
 		</div>
 		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+14" alt="Xe 14">
+			<a href="/cart"><img src="https://via.placeholder.com/300x200.png?text=Xe+15" alt="Xe 15"></a>
 			<div class="car-info">
-				<h4>Xe 14</h4>
+				<h4>Xe 15</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				<p class="price">Giá thuê: $300/ngày</p>
 			</div>
 		</div>
-		<div class="car-card">
-			<img src="https://via.placeholder.com/300x200.png?text=Xe+15" alt="Xe 15">
-			<div class="car-info">
-				<h4>Xe 15</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-				<p class="price">Giá thuê: $350/ngày</p>
-			</div>
 		</div>
 	</div>
 </div>
